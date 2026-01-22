507e8927130d88d0f8ea5287a09eb6d5ad25c8be
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-01-22 16:21:39 -0600

    agrege el escaner

---
+++ Meses.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Meses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number");
+        int number = sc.nextInt();
+        
+
+
+
+
+
+
+
+
+
+        
+
+
+
+    }
+
+
+
+
+
+
+}
+
+    
